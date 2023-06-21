d350e74b29f1fffb55cdec537ab448803de1f6bf
Christian Banse <oxisto@clouditor.io>
2023-06-21 17:47:01 +0200

    Implemented sidebar with searchbar with tailwind (#7)
    * Implemented sidebar with searchbar with tailwind
    
    Closes #6
    
    * Re-organized navigation bar

---
+++ src/lib/components/NavigationItem.svelte
@@ -0,0 +1,57 @@
+<script context="module" lang="ts">
+  import { Icon, type IconSource } from '@steeze-ui/svelte-icon';
+  import { page } from '$app/stores';
+  import NavigationItem from '$lib/components/NavigationItem.svelte';
+
+  /**
+   * An interface that contains necessary data to display a navigation item.
+   */
+  export interface NavigationItemData {
+    name: string;
+    href: string;
+    icon?: IconSource;
+    isSub?: boolean;
+    isNew?: boolean;
+    children?: NavigationItemData[];
+  }
+</script>
+
+<script lang="ts">
+  /**
+   * The data for this navigation item.
+   */
+  export let item: NavigationItemData;
+
+  // React to changes in the route and set the current status (true/false)
+  $: current = $page.url.pathname.startsWith(item.href);
+</script>
+
+<li>
+  <a
+    href={item.href}
+    class="
+      {current
+      ? 'bg-gray-50 text-clouditor'
+      : 'text-gray-700 hover:text-clouditor hover:bg-gray-50'}
+      {item.isSub
+      ? 'block rounded-md py-2 pr-2 pl-9 text-sm leading-6'
+      : 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'}
+      {item.isNew ? 'border-t' : ''}
+      "
+  >
+    {#if item.icon}
+      <Icon
+        src={item.icon}
+        class="{current ? 'text-clouditor' : 'text-gray-400 group-hover:text-clouditor'}
+        h-6 w-6 shrink-0"
+        aria-hidden="true"
+      />
+    {/if}
+    {item.name}
+  </a>
+  <ul class="mt-1 px-2">
+    {#each item.children ?? [] as subItem}
+      <NavigationItem item={subItem} />
+    {/each}
+  </ul>
+</li>
---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import type { NavigationItemData } from '$lib/components/NavigationItem.svelte';
+  import NavigationItem from '$lib/components/NavigationItem.svelte';
+  import type { CloudService } from '$lib/orchestrator';
+  import {
+    AdjustmentsHorizontal,
+    ArchiveBox,
+    Calendar,
+    ChartPie,
+    Cog6Tooth,
+    Folder,
+    Home
+  } from '@steeze-ui/heroicons';
+  import { Icon } from '@steeze-ui/svelte-icon';
+
+  export let services: CloudService[];
+  export let mobile: boolean = false;
+
+  // Build navigation menu with dynamic components
+  const navigation: NavigationItemData[] = [
+    { name: 'Dashboard', href: '/dashboard', icon: Home },
+    {
+      name: 'Cloud Services',
+      href: '/cloud',
+      icon: Folder,
+      children: [
+        ...services.map((s) => {
+          return {
+            name: s.name,
+            href: '/cloud/' + s.id,
+            isSub: true
+          };
+        }),
+        {
+          name: 'New...',
+          href: '/cloud/new',
+          isSub: true,
+          isNew: true
+        }
+      ]
+    },
+    { name: 'Metrics', href: '/metrics', icon: AdjustmentsHorizontal },
+    { name: 'Catalog Data', href: '/catalogs', icon: ArchiveBox },
+    { name: 'Reports', href: '/reports', icon: ChartPie }
+  ];
+</script>
+
+<div
+  class="{mobile ? '' : 'border-r border-gray-200'}
+    flex grow flex-col gap-y-5 overflow-y-auto bg-slate-50 px-6 pb-4"
+>
+  <div class="flex h-16 shrink-0 items-center border-b border-gray-200">
+    <img
+      class="h-12 w-auto"
+      style="transform: scaleX(-1);"
+      src="/img/logo-small2.png"
+      alt="The Clouditor"
+    />
+    <div class="ml-4 flex flex-col">
+      <div class="text-xl">Clouditor</div>
+      <div class="text-sm text-gray-500">The Cloud Auditor</div>
+    </div>
+  </div>
+  <nav class="flex flex-1 flex-col">
+    <ul class="flex flex-1 flex-col gap-y-7">
+      <li>
+        <ul class="-mx-2 space-y-1">
+          {#each navigation as item}
+            <NavigationItem {item} />
+          {/each}
+        </ul>
+      </li>
+
+      <li class="mt-auto">
+        <a
+          href="/"
+          class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-clouditor"
+        >
+          <Icon
+            src={Cog6Tooth}
+            class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-clouditor"
+            aria-hidden="true"
+          />
+          Settings
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
