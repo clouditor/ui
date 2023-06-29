d41583353a4b18ea74ffcc44003199dbbdff7ccf
Tom Mirwald <40235770+70mm1@users.noreply.github.com>
2023-06-29 17:01:58 +0200

    Add simple Table to display Metrics

---
+++ src/routes/metrics/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<h1 class="text-3xl font-bold underline">Metrics</h1>
+
+<div class="px-4 sm:px-6 lg:px-8">
+  <div class="sm:flex sm:items-center">
+    <div class="sm:flex-auto">
+      <p class="mt-2 text-sm text-gray-700">A list of all the available metrics.</p>
+    </div>
+    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none" />
+  </div>
+  <div class="mt-8 flow-root">
+    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <table class="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                >ID</th
+              >
+              <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >Name</th
+              >
+              <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >Category</th
+              >
+            </tr>
+          </thead>
+
+          <tbody>
+            {#each data.metrics as metric, i}
+              <tr class={i % 2 == 0 ? 'bg-white' : 'bg-gray-50'}>
+                <td class="px-3 py-3.5 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {metric.id}
+                </td>
+                <td class="px-3 py-3.5 whitespace-nowrap text-sm text-gray-500">
+                  {metric.name}
+                </td>
+                <td class="px-3 py-3.5 whitespace-nowrap text-sm text-gray-500">
+                  {metric.category}
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
---
+++ src/routes/metrics/+page.ts
@@ -0,0 +1,9 @@
+import { listMetrics } from '$lib/api/orchestrator';
+
+export const load = async () => {
+    const metrics = await listMetrics();
+    
+    return {
+        metrics: metrics
+    };
+}
