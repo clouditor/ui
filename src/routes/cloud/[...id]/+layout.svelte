3ae0e2a9e6cde99d24f7ec6ca0117ea1b5a86110
Christian Banse <oxisto@aybaze.com>
2023-07-04 09:19:12 +0200

    More detail

--- src/routes/cloud/[...id]/+layout.svelte
+++ src/routes/cloud/[...id]/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
+  import { removeCloudService } from '$lib/api/orchestrator';
+  import Header from '$lib/components/Header.svelte';
   import Tabs from '$lib/components/Tabs.svelte';
   import {
     BuildingOffice,
@@ -8,9 +11,6 @@
     Users
   } from '@steeze-ui/heroicons';
   import type { PageData } from './$types';
-  import Button from '$lib/components/Button.svelte';
-  import { removeCloudService } from '$lib/api/orchestrator';
-  import { goto } from '$app/navigation';
 
   export let data: PageData;
 
@@ -50,36 +50,15 @@
   }
 </script>
 
-<div class="md:flex md:items-center md:justify-between md:space-x-5">
-  <div class="flex items-start space-x-5">
-    <div class="flex-shrink-0">
-      <div class="relative">
-        <img
-          class="h-16 w-16 rounded-full"
-          src="https://images.unsplash.com/photo-1499346030926-9a72daac6c63?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-          alt=""
-        />
-        <span class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true" />
-      </div>
-    </div>
-    <!--
-      Use vertical padding to simulate center alignment when both lines of text are one line,
-      but preserve the same layout if the text wraps without making the image jump around.
-    -->
-    <div class="pt-1.5">
-      <h1 class="text-2xl font-bold text-gray-900">{data.service.name}</h1>
-      <p class="text-sm font-medium text-gray-500">
-        {data.statistics.discoveredResources} discovered resources
-      </p>
-    </div>
-  </div>
-  <div
-    class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3"
-  >
-    <Button on:click={remove} class="bg-red-800 hover:bg-red-700">Delete</Button>
-    <Button>Save</Button>
-  </div>
-</div>
+<Header
+  name={data.service.name}
+  description={[
+    data.service.description,
+    data.statistics.discoveredResources + ' discovered resources'
+  ]
+    .filter((s) => s != '')
+    .join('. ')}
+/>
 
 <Tabs items={tabs} />
 
