7f14ac08366f4cf1f09562e8ee8d04c3b650fa34
Christian Banse <oxisto@aybaze.com>
2023-07-11 09:55:17 +0200

    hover

--- src/lib/components/AddEvaluationResultDialog.svelte
+++ src/lib/components/AddEvaluationResultDialog.svelte
@@ -95,7 +95,7 @@
                   <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                     <button
                       type="button"
-                      class="inline-flex w-full justify-center rounded-md bg-clouditor px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clouditor sm:col-start-2"
+                      class="inline-flex w-full justify-center rounded-md bg-clouditor px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-clouditor-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clouditor sm:col-start-2"
                       on:click={submit}
                     >
                       Save
