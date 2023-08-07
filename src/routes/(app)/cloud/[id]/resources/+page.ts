e635b34526d90033851774f31dd498704ad80920
Tom Mirwald <40235770+70mm1@users.noreply.github.com>
2023-08-07 14:54:45 +0200

    Add results as PageData

---
+++ src/routes/(app)/cloud/[id]/resources/+page.ts
@@ -0,0 +1,17 @@
+import { listCloudServiceAssessmentResults } from '$lib/api/orchestrator';
+
+import { error } from '@sveltejs/kit';
+
+import type { PageLoad } from './$types';
+
+export const load = (async ({ fetch, params, url }) => {
+  if (params.id == undefined) {
+    throw error(405, 'Required parameter missing');
+  }
+
+  const results = await listCloudServiceAssessmentResults(params.id, fetch);
+
+  return {
+    results
+  };
+}) satisfies PageLoad;
