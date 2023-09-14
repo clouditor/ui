032bbb723b2664724628b51d69e5e8681430fc92
Tom Mirwald <40235770+70mm1@users.noreply.github.com>
2023-09-14 18:36:59 +0200

    Add logic to get two values from one URL

---
+++ src/routes/(app)/metrics/[...metricId]/+page.ts
@@ -0,0 +1,24 @@
+import { getMetric, getMetricConfiguration } from '$lib/api/orchestrator';
+
+import { error } from '@sveltejs/kit';
+
+import type { PageLoad } from './$types';
+
+export const load = (async ({ params }) => {
+  console.log(params);
+  if (params.metricId.split('/').length < 2) {
+    throw error(405, 'Required parameter missing');
+  }
+
+  // slugs[0] is the serviceId
+  // slugs[1] is the metricId
+
+  const slugs = params.metricId.split('/');
+  const metric = await getMetric(slugs[1]);
+  const metricConfiguration = await getMetricConfiguration(slugs[0], slugs[1]);
+
+  return {
+    metric,
+    metricConfiguration
+  };
+}) satisfies PageLoad;
