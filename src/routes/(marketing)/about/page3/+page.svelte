003e89c4505808eeffea921275b9506f05d5faec
Nico Haas <nico.haas@aisec.fraunhofer.de>
2023-11-03 10:10:39 +0100

    Add hollistic view slide

--- src/routes/(marketing)/about/page3/+page.svelte
+++ src/routes/(marketing)/about/page3/+page.svelte
@@ -1,1 +1,8 @@
-Page 3
+<a href="/cloud">
+    <img
+      src="/img/holisticslide.png"
+      alt="Product screenshot"
+      style="margin-top: 100px;top: 200;transform: scale(0.9);"
+    />
+</a>
+  
