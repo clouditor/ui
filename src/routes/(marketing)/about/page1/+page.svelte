ffa025dc514765760efbe7e7816e3a35a5d2c88f
Nico Haas <nico.haas@aisec.fraunhofer.de>
2023-11-03 10:06:30 +0100

    Add future-proof-slide

--- src/routes/(marketing)/about/page1/+page.svelte
+++ src/routes/(marketing)/about/page1/+page.svelte
@@ -1,1 +1,8 @@
-Page 1
+<a href="/cloud">
+    <img
+      src="/img/futureproofslide.png"
+      alt="Product screenshot"
+      style="margin-top: 60px;top: 200;transform: scale(0.9);"
+    />
+  </a>
+  
