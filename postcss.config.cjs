527850c7a601da638f2a4f47b71eddebc4200b34
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2025-07-25 13:03:36 +0200

    Revert "Bump tailwindcss from 3.4.17 to 4.1.11 (#250)"
    This reverts commit ddd232de2a06e551c67dbe0ea8b502cb594a4f76.

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	plugins: [
-	  require('@tailwindcss/postcss')({     // neu
-		config: './tailwind.config.js',     // Pfad zu deiner Tailwind-Config
-	  }),
-	  require('autoprefixer'),
-	],
-  };
